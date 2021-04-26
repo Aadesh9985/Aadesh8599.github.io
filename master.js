bcc680f6d52f2ccfad1b6866197accd369a7fb84
A A D E S H ___G U R A V <guravaadesh99@gmail.com>
2021-04-27 01:09:16 +0530

    Update master.js

--- master.js
+++ master.js
@@ -1,5 +1,7 @@
 let progressValue = 0;
 const progressBar = document.querySelector(".current-progress");
+const pbg = document.querySelector(".progress")
+const ag = document.querySelector(".ag");
 
 progressBar.style.width = `${progressValue}%`;
 
@@ -10,5 +12,11 @@ const timer = setInterval(() => {
   }
   if (progressValue === 100) {
     clearInterval(timer);
+    document.querySelector(".coming--soon").classList.remove("hide");
+    document.querySelector(".pg").classList.remove("hide");
+    ag.classList.add("animate__animated");
+    ag.classList.add("animate__fadeOut");
+    progressBar.classList.add("hide");
+    pbg.classList.add("hide");
   }
 }, 1000);
